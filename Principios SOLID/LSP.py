ee8eae33759abf750d847e99675ee235db607943
Santiago Fontana <tatifontana14@gmail.com>
2024-10-17 20:40:10 -0300

    Agregando los archivos: SRP, OCP, LSP y ISP

---
+++ Principios SOLID/LSP.py
@@ -0,0 +1,22 @@
+# class Ave:
+#     def volar(self):
+#         return "Esoy volando"
+    
+# class Pinguino(Ave):
+#     def volar(seldf):
+#         return "No puedo volar"
+    
+# def hacer_volar(ave = Ave):
+#     return ave.volar()
+
+# print(hacer_volar(Pinguino()))
+
+class Ave:
+    pass
+
+class AveVoladora(Ave):
+    def volar(self):
+        return "Estoy volando"
+    
+class AveNoVoladora(Ave):
+    pass
